3226376df6da79c7b8a6f6e7a4bdbc92c2443414
dong hu <dickhu@hotmail.com>
2018-02-23 16:33:25 -0500

    upd

--- rand/rand.c
+++ rand/rand.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main () {
-   int i, n, r1,r2,count=0;
+   int i, n, r1,r2,count=0,count1=0,count2=0;
    time_t t;
    
    n = 1000000000;
@@ -16,10 +16,14 @@ int main () {
         r1=rand() % 100;
         r2=rand() % 100;
        //printf("%d\n", r);
-       if ( abs(r1-r2) > 50 )  count++  ;
-       //if ( r1 < 50 )  count++  ;
+       if ( abs(r1-r2) < 50 )  count++  ;
+       //if ( r1 == 0 )  count++  ;
+       //if ( r1 == 50 )  count1++  ;
+       //if ( r1 == 99 )  count2++  ;
    }
    
    printf("count=%d\n", count);
+   //printf("count1=%d\n", count1);
+   //printf("count2=%d\n", count2);
    return(0);
 }
