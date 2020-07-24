5b9fa1a7e87aee41b99cc529a16b5483528aaa40
Dong Hu <dong.hu@sickkids.ca>
2020-07-24 17:33:09 -0400

    update

--- malloc-i.c
+++ malloc-i.c
@@ -2,20 +2,23 @@
 #include<stdlib.h>
 #include<string.h>
 #define BLOCK_SIZE 1024*1024*1024
+
 int main(int argc, char **argv)
 {
 
         int i=0;
 	int totalgb;
-        char *buf[900];
+        char *buf[5000];   //assume max up to 5000GB
 
 	if(argc != 2) {
-		printf("Usage: %s\t number of GB to allocate\n", argv[0]);
+		printf("Usage: %s\t number of GB (up to 5000) to allocate\n", argv[0]);
 		puts("example: ./a.out 5    	#to allocate 5gb\n\n");
 		return 1;
 	}
 	
 	totalgb=atoi(argv[1]);
+        printf ("allocating %dGB of mem, 1GB at one time\n\n", totalgb);
+
 
         while(i<totalgb)
         {
